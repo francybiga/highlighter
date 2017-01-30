7abaadd0717875dac10cdd4544c0cc0bf4d6daa0
Francesco Bigagnoli <francesco.bigagnoli@scmitaly.it>
2017-01-30 19:04:41 +0100

    added logs using logging module

--- script/evernoteHighlighter.py
+++ script/evernoteHighlighter.py
@@ -2,6 +2,7 @@
 
 import yaml
 import os
+import logging
 
 from matcher import match_highlight_file_to_note
 
@@ -20,7 +21,10 @@ def copyNoteWithNewContent(original_note, content, notebook_guid):
         return hl_note
 
 def main():
-        #Load configuration
+
+    logging.basicConfig(format='%(message)s', level=logging.INFO)
+
+    #Load configuration
     config = yaml.load(open('config.yml', 'r'))
     highlights_dir = config['highlights_dir']
     highlights_backup_dir = highlights_dir + 'backup/'
@@ -28,6 +32,17 @@ def main():
     output_notebook_name = config['output_notebook_name']
     backup_notebook_name = config['backup_notebook_name']
     dev_token = config['dev_token']
+
+    logging.info('----------------------------')
+    logging.info('Configuration:')
+    logging.info('----------------------------')
+    logging.info('highlights_dir: %s', highlights_dir)
+    logging.info('highlights_backup_dir: %s', highlights_backup_dir)
+    logging.info('input_notebook_name: %s', input_notebook_name)
+    logging.info('output_notebook_name: %s', output_notebook_name)
+    logging.info('backup_notebook_name: %s', backup_notebook_name)
+    logging.info('dev_token %s', dev_token)
+    logging.info('----------------------------\n')
 
     #Initialize Evernote stack
     client = EvernoteClient(token=dev_token)
@@ -55,39 +70,54 @@ def main():
 
     notes_meta_list = note_store.findNotesMetadata(dev_token, search_filter, 0, 100, notes_metadata_spec).notes
 
-    for hl_filename in [name for name in os.listdir(highlights_dir) if name.endswith('.txt')]:
+    hl_files = [name for name in os.listdir(highlights_dir) if name.endswith('.txt')]
+    if len(hl_files) == 0:
+        logging.info('zero highlight text file found')
+        return
+
+    matched_notes = 0
+    new_notes_titles = []
+    for hl_filename in hl_files:
         hl_filename_no_ext = os.path.splitext(hl_filename)[0]
-    for note_meta in notes_meta_list:
-        note_transformed_title = note_meta.title.replace(' ', '_').lower()
-        if note_transformed_title == hl_filename_no_ext: 
-            #print 'Found matching notename: '
-            #print hl_filename
-            #print note_meta.guid + ' : ' + note_meta.title
+        for note_meta in notes_meta_list:
+            note_transformed_title = note_meta.title.replace(' ', '_').lower()
+            if note_transformed_title == hl_filename_no_ext: 
+                matched_notes += 1
+                logging.info('Matching note found:')
+                logging.info('title: %s', note_meta.title)
+                logging.info('hl text file: %s\n', hl_filename_no_ext)
 
-            original_note = note_store.getNote(dev_token, note_meta.guid, True, False, False, False)
+                original_note = note_store.getNote(dev_token, note_meta.guid, True, False, False, False)
 
-            #match highlight in note content
-            os.chdir(highlights_dir)
-            hl_file_path = os.path.abspath(hl_filename)
-            hl_content = match_highlight_file_to_note(hl_file_path, original_note.content)
-            
-            #create new note with the highlighted content and save it to the output notebook
-            hl_note = copyNoteWithNewContent(original_note, hl_content, out_notebook_guid)
-            note_store.createNote(dev_token, hl_note)
-            
-            #move old note to the backup notebook (if present)
-            if backup_notebook_guid:
-                original_note.notebookGuid = backup_notebook_guid
-                note_store.updateNote(dev_token, original_note)
+                #match highlight in note content
+                os.chdir(highlights_dir)
+                hl_file_path = os.path.abspath(hl_filename)
+                hl_content = match_highlight_file_to_note(hl_file_path, original_note.content)
+                
+                #create new note with the highlighted content and save it to the output notebook
+                hl_note = copyNoteWithNewContent(original_note, hl_content, out_notebook_guid)
+                note_store.createNote(dev_token, hl_note)
+                logging.info('created new higlighted note: %s', hl_note.title)
+                new_notes_titles.append(hl_note.title) 
 
-            #move the highlight text file to a proper subfolder 
-            if not os.path.isdir(highlights_backup_dir):
-                os.mkdir(highlights_backup_dir)
-            hl_backup_file_path = highlights_backup_dir + hl_filename
-            os.rename(hl_file_path, hl_backup_file_path)
-            print hl_file_path + '\n->\n' + hl_backup_file_path
+                #move old note to the backup notebook (if present)
+                if backup_notebook_guid:
+                    original_note.notebookGuid = backup_notebook_guid
+                    note_store.updateNote(dev_token, original_note)
+                    logging.info('moved original note to the backup notebook')
 
-            #add meaningful log messages for each step using the log utilities (move them to another "utils" file?)
+                #move the highlight text file to a proper subfolder 
+                if not os.path.isdir(highlights_backup_dir):
+                    logging.info('creating backup dir at: %s', highlights_backup_dir)
+                    os.mkdir(highlights_backup_dir)
+                hl_backup_file_path = highlights_backup_dir + hl_filename
+                os.rename(hl_file_path, hl_backup_file_path)
+                logging.info('moved highlight text file to backup folder')
+    logging.info('----------------------------')
+    logging.info('Done! %d notes matched', matched_notes)
+    for title in new_notes_titles:
+        logging.info('\t* %s', title)
+    logging.info('----------------------------')
 
 # Standard boilerplate to call the main() function to begin
 # the program.
--- script/matcher.py
+++ script/matcher.py
@@ -1,26 +1,6 @@
 #!/usr/bin/env python
 
-class Log:
-    info = 1
-    debug = 2
-
-log_level = Log.info
-log = ''
-
-def log_append(string, level=Log.info):
-    global log
-    if level <= log_level:
-        log += string + '\n'
-
-def log_flush():
-    global log
-    if len(log) > 0:
-        print log
-        log = ''
-
-def log_delete():
-    global log
-    log = ''
+import logging
 
 def match_highlight(string, text):
     if len(string) > len(text): return None
@@ -28,9 +8,9 @@ def match_highlight(string, text):
     matching_idxs = []
     start_idx = text.find(string[0], 0) #start with the first match of the first character of the highlight
     while start_idx != -1: #if the first character match, go ahead matching all the subsequent chars of string
-        log_append('highlight: "' + string + '"', Log.debug)
-        log_append('moving to next occurrence at index: (%d, %d)' % (start_idx, start_idx + len(string)), Log.debug)
-        log_append('---\n(%d,%d): %s \n---' % (start_idx, start_idx + len(string), text[start_idx:start_idx + len(string)]), Log.debug)
+        logging.debug('highlight: "' + string + '"')
+        logging.debug('moving to next occurrence at index: (%d, %d)' % (start_idx, start_idx + len(string)))
+        logging.debug('---\n(%d,%d): %s \n---' % (start_idx, start_idx + len(string), text[start_idx:start_idx + len(string)]))
         end_idx = None 
         tag_offset = 0
         str_idx = 0
@@ -38,30 +18,28 @@ def match_highlight(string, text):
             text_idx = start_idx + str_idx + tag_offset
             if string[str_idx] == text[text_idx]:
                 end_idx = text_idx
-                log_append(string[str_idx] + '(' + str(str_idx) + ')' + '==' + text[text_idx] + '(' + str(text_idx) + ')', Log.debug)
+                logging.debug(string[str_idx] + '(' + str(str_idx) + ')' + '==' + text[text_idx] + '(' + str(text_idx) + ')')
                 str_idx += 1
             else:
                 if text[text_idx] == '<': #if we find a '<' character (not present in the highlight string, we search for the tag close character and, using the tag offset variable, we "jump" till the end of the tag to continue the matching attempt
-                    log_append('starting tag at ' + str(text_idx), Log.debug)
+                    logging.debug('starting tag at ' + str(text_idx))
                     tag_end_idx = text.find('>', text_idx)
                     if tag_end_idx != -1:
-                        log_append('found tag at ' + str(text_idx) + '-' + str(tag_end_idx), Log.debug)
+                        logging.debug('found tag at ' + str(text_idx) + '-' + str(tag_end_idx))
                         tag_offset += tag_end_idx - text_idx + 1
                 else:
                     end_idx = None
-                    log_delete()
-                    log_append('BUM - ' + string[str_idx] + '(' + str(str_idx) + ')' + '!=' + text[text_idx] + '(' + str(text_idx) + ')', Log.debug)
+                    logging.debug('BUM - ' + string[str_idx] + '(' + str(str_idx) + ')' + '!=' + text[text_idx] + '(' + str(text_idx) + ')')
                     start_idx = text.find(string[0], start_idx + 1) #compute the next matching index of the first character to begin a new matching series attempt
                     break     #when you encounter a character not matching the matching series is broken, thus go ahead breaking the loop
         if end_idx != None:     
             hl_opening_tag = '<span style="background-color: rgb(255, 250, 165);-evernote-highlight:true">'
             hl_closing_tag = '</span>'
-            log_append('old text: ' + text[start_idx:end_idx+1], Log.debug)
+            logging.debug('old text: ' + text[start_idx:end_idx+1])
             text = text[:start_idx] + hl_opening_tag + text[start_idx:end_idx+1] + hl_closing_tag + text[end_idx+1:]
             end_idx = end_idx + len(hl_opening_tag) + len(hl_closing_tag)#we should recompute the end idx adding the size of the opening tag
-            log_append('new text: ' + text[start_idx:end_idx + 1], Log.debug)
-            log_append('starting new search from index ' + str(end_idx), Log.debug)
-            log_flush()
+            logging.debug('new text: ' + text[start_idx:end_idx + 1])
+            logging.debug('starting new search from index ' + str(end_idx))
             start_idx = text.find(string[0], end_idx)
     return text 
 
